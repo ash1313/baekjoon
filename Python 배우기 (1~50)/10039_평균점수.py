01d73bdeab874412785c6c8e1a700525c35b1890
unknown <ash1313@koreatech.ac.kr>
2022-06-29 14:32:29 +0900

    9

---
+++ Python 배우기 (1~50)/10039_평균점수.py
@@ -0,0 +1,18 @@
+t1 = int(input())
+t2 = int(input())
+t3 = int(input())
+t4 = int(input())
+t5 = int(input())
+
+if(t1 < 40):
+    t1 = 40
+if(t2 < 40):
+    t2 = 40
+if(t3 < 40):
+    t3 = 40
+if(t4 < 40):
+    t4 = 40
+if(t5 < 40):
+    t5 = 40
+
+print(int((t1+t2+t3+t4+t5)/5)) 
---
+++ Python 배우기 (1~50)/1934_최소공배수.py
@@ -0,0 +1,14 @@
+j= int(input(''))
+
+
+
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a,b)
+for i in range(j):
+  a,b = map(int,input().split(" "))
+  print(lcm(a,b))
